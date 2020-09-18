63aaeb005a14534e14717afa6ec1f64510cbee20
Robrecht Cannoodt <rcannood@gmail.com>
2020-09-18 11:22:14 +0200

    preserve yaml by default

--- examples/knit/code.R
+++ examples/knit/code.R
@@ -1,15 +1,7 @@
-### VIASH START
-par <- list(
-  input = "examples/knit/tests/input.Rmd",
-  output = "examples/knit/tests/input.md"
-)
-resources_dir <- "."
-### VIASH END
-
 library(rmarkdown)
 render(
   input = par$input,
-  output_format = github_document(html_preview = FALSE),
+  output_format = md_document(preserve_yaml = par$preserve_yaml),
   output_file = basename(par$output),
   output_dir = dirname(par$output)
 )
