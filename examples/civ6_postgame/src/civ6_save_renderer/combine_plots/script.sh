458fc4759ed12b6dd267da8b8b253ce42f36cb74
Robrecht Cannoodt <rcannood@gmail.com>
2020-10-22 20:30:04 +0200

    fix ffmpeg command

--- examples/civ6_postgame/src/civ6_save_renderer/combine_plots/script.sh
+++ examples/civ6_postgame/src/civ6_save_renderer/combine_plots/script.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-#inputs=`echo $par_input | sed "s#:#' -i '#g"`
-#eval ffmpeg -framerate $par_framerate -f image2 -i "'$inputs'" -c:v libvpx-vp9 -pix_fmt yuva420p -y "'$par_output'"
-
-ffmpeg -f concat -safe 0 -i <(echo $par_input | tr ':' '\n' | sed "s#.*#file &#") -framerate $par_framerate -c:v libvpx-vp9 -pix_fmt yuva420p -y "$par_output"
+ffmpeg -framerate $par_framerate -i "concat:$(echo $par_input | tr ':' '|')" -c:v libvpx-vp9 -pix_fmt yuva420p -y "$par_output"
