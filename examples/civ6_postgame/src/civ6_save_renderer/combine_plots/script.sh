362c40d75c4d0fd46363e798bd17e60a46778fd6
Robrecht Cannoodt <rcannood@gmail.com>
2020-10-22 20:12:44 +0200

    set ffmpeg to multiple

--- examples/civ6_postgame/src/civ6_save_renderer/combine_plots/script.sh
+++ examples/civ6_postgame/src/civ6_save_renderer/combine_plots/script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# render movie
-ffmpeg -framerate $par_framerate -f image2 -i "$par_input/%*.png" -c:v libvpx-vp9 -pix_fmt yuva420p -y "$par_output"
+#inputs=`echo $par_input | sed "s#:#' -i '#g"`
+#eval ffmpeg -framerate $par_framerate -f image2 -i "'$inputs'" -c:v libvpx-vp9 -pix_fmt yuva420p -y "'$par_output'"
+
+ffmpeg -f concat -safe 0 -i <(echo $par_input | tr ':' '\n' | sed "s#.*#file &#") -framerate $par_framerate -c:v libvpx-vp9 -pix_fmt yuva420p -y "$par_output"
