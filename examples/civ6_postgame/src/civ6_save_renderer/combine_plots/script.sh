913ed48a3abdcfb7d6631009291c60c37dddcf4b
Robrecht Cannoodt <rcannood@gmail.com>
2021-02-02 14:06:04 +0100

    update components

--- examples/civ6_postgame/src/civ6_save_renderer/combine_plots/script.sh
+++ examples/civ6_postgame/src/civ6_save_renderer/combine_plots/script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ffmpeg -framerate $par_framerate -i "concat:$(echo $par_input | tr ':' '|')" -c:v libvpx-vp9 -pix_fmt yuva420p -y "$par_output"
+inputs=$(echo $par_input | tr ':' '|')
+ffmpeg -framerate $par_framerate -i "concat:$inputs" -c:v libvpx-vp9 -pix_fmt yuva420p -y "$par_output"
