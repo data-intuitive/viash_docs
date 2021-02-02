b778a45bb7d57b1a50307873e810c6a1cbe9d0d7
Robrecht Cannoodt <rcannood@gmail.com>
2021-02-02 14:05:49 +0100

    allow toggling the format

--- examples/knit/code.R
+++ examples/knit/code.R
@@ -1,7 +1,14 @@
 library(rmarkdown)
+
+if (par$format == "md_format") {
+  par$format <- md_document(preserve_yaml = par$preserve_yaml)
+} else if (par$format == "github_document") {
+  par$format <- github_document(html_preview = FALSE)
+}
+
 render(
   input = par$input,
-  output_format = md_document(preserve_yaml = par$preserve_yaml),
+  output_format = par$format,
   output_file = basename(par$output),
   output_dir = dirname(par$output)
 )
