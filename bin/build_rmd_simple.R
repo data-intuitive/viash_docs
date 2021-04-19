a63ba707073c54a5a01a184e5082fc3c01f622e4
Eric Van de Kerckhove <BlackDragonBE@users.noreply.github.com>
2021-04-19 13:02:37 +0200

    Added simple rmd conversion script as a fallback if the viash generated one isn't working

---
+++ bin/build_rmd_simple.R
@@ -0,0 +1,32 @@
+# Rmd files under the root directory
+rmds <- c(
+  list.files(".", '[.]Rmd$', recursive = TRUE, full.names = TRUE)
+)
+
+# remove rmds under examples dir
+rmds <- rmds[!grepl("^\\./examples|^\\./_site", rmds)]
+
+# get corresponding mds
+mds <- blogdown:::with_ext(rmds, '.md')
+
+
+for (i in seq_along(rmds)) {
+  rmd <- rmds[[i]]
+  md <- mds[[i]]
+
+
+  # if output is not older than input, skip the compilation
+  if (blogdown:::require_rebuild(md, rmd)) {
+    message('* knitting ', rmd)
+
+    rmarkdown::render(
+      input = rmd,
+      output_format = rmarkdown::md_document(
+        variant = "gfm",
+        preserve_yaml = TRUE
+      )
+    )
+  }
+}
+
+message('Finished knitting Rmd files.')
