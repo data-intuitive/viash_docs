07e6dfac16d290935ad55fcc1b1b55a59eb4e491
Robrecht Cannoodt <rcannood@gmail.com>
2021-05-28 14:04:57 +0200

    refactor script

--- examples/md_url_checker_r/script.R
+++ examples/md_url_checker_r/script.R
@@ -1,8 +1,9 @@
-library(httr)
-library(knitr)
-library(rvest)
 
-# Depends on tidyverse r package
+options(tidyverse.quiet = TRUE)
+library(tidyverse)
+library(httr, quietly = TRUE)
+library(rvest, quietly = TRUE)
+
 # Depends on pandoc local package
 
 ### 1 ###
@@ -10,18 +11,19 @@ library(rvest)
 ## VIASH START
 par <- list(
   inputfile = "Testfile.md",
-  domain = "http://www.data-intuitive.com/viash_docs",
-  output = "output.txt",
-  temp_html = "temp.html"
+  domain = "https://viash.io",
+  output = "output.txt"
 )
-
-knitr::opts_chunk$set(message = FALSE)
 
 ## VIASH END
 
+temp_html <- tempfile(fileext = ".html")
+on.exit(file.remove(temp_html)) # remove tempfile after scripts exits to make sure it's always removed
+
+
 # Convert the markdown file to html
-rmarkdown::render(input = par$inputfile, output_format = "html_document", output_file = par$temp_html, quiet = TRUE, runtime="static")
-html <- read_html(par$temp_html)
+rmarkdown::render(input = par$inputfile, output_format = "html_document", output_file = temp_html, quiet = TRUE, runtime="static")
+html <- rvest::read_html(temp_html)
 
 cat("Extracting URLs\n")
 
@@ -34,69 +36,65 @@ titles <- html %>% html_elements("a") %>%
   html_text()
 
 # Convert the markdown file to html
-amount_of_urls <- length(urls)
-cat("Checking", amount_of_urls, "URLs\n")
-
-# Clear file
-write("",file=par$output)
+cat("Checking", length(urls), "URLs\n")
 
 amount_of_errors <- 0
-index <- 0
 expected_code <- "200"
 
 ### 3 ###
 
 # Iterate over the array of URLs and check each of them
-for (i in urls){
-  url <- urls[index+1]
-  title <- titles[index+1]
+outputs <- map_df(seq_along(urls), function(i) {
+  url <- urls[i]
+  title <- titles[i]
 
   ### 4 ###
 
   # If an URL doesn't start with 'http', add the domain before it
-  if (!grepl( "http", url, fixed = TRUE)) {
+  if (!grepl("^https?://", url)) {
     url = paste0(par$domain, url)
   }
 
-  message(paste(index+1, url, sep = ": "))
+  cat(i, ": ", url, "\n", sep = "")
 
-  write(paste("Link name:", title),file=par$output, append = TRUE)
-  write(paste("URL:", url),file=par$output, append = TRUE)
+  output <- tibble(
+    url,
+    title,
+  )
 
   ### 5 ###
 
   # Do a web request and get the status code
-  tryCatch(
-    {
-      r <- GET(url)
-      statuscode <- status_code(r)
-
-      # Check if status code obtained via cURL contains the expected code
-      if (statuscode == expected_code) {
-        message("OK")
-        write("OK", file=par$output, append = TRUE)
-      }  else {
-        message(statuscode)
-        write(paste("ERROR! URL cannot be reached. Status code:", statuscode),file=par$output, append = TRUE)
-        amount_of_errors = amount_of_errors + 1
+  output$status <-
+    tryCatch({
+      code <- status_code(GET(url))
+      if (code == expected_code) {
+        "OK"
+      } else {
+        paste0("ERROR! URL cannot be reached. Status code:")
       }
     },
-    error=function(cond) {
-      message(paste("URL does not seem to exist!"))
-      write("ERROR! URL does not seem to exist!", file=par$output, append = TRUE)
-      amount_of_errors = amount_of_errors + 1
+    error = function(cond) {
+      "ERROR! URL does not seem to exist!"
     },
-    warning=function(cond) {
-      message(paste("URL caused a warning!"))
-      write("ERROR! URL caused a warning", file=par$output, append = TRUE)
-      amount_of_errors = amount_of_errors + 1
-    }
-  )
+    warning = function(cond) {
+      "ERROR! URL caused a warning!"
+    })
 
-  write("---", file=par$output, append = TRUE)
-  index = index+1
-}
+  output
+})
 
-message("")
-message(paste(par$inputfile, "has been checked and a report named", par$output, "has been generated."))
-message(paste(amount_of_errors, "of", amount_of_urls, "URLs could not be resolved."))
+print(outputs)
+
+content <- paste0(
+  "Link name: ", outputs$title, "\n",
+  "URL: ", outputs$url, "\n",
+  outputs$status, "\n",
+  "---\n"
+)
+
+write_lines(content, par$output)
+
+cat("")
+cat("Input '", par$inputfile, "' has been checked and a report named '", par$output, "' has been generated.\n", sep = "")
+cat(sum(outputs$status != "OK"), " out of ", nrow(outputs), " URLs could not be reached.\n", sep = "")
